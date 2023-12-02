bea63b862a65dc7069e6213497e22b1bcde5e2a4
aniindyta <aninditaputri055@gmail.com>
2023-12-02 08:22:12 +0700

    define local database, dao, room database, implement repository

--- app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/DataRepository.kt
@@ -2,38 +2,41 @@ package com.dicoding.courseschedule.data
 
 import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.paging.Config
 import androidx.paging.PagedList
+import androidx.paging.toLiveData
 import com.dicoding.courseschedule.util.QueryType
 import com.dicoding.courseschedule.util.QueryUtil.nearestQuery
+import com.dicoding.courseschedule.util.QueryUtil.sortedQuery
 import com.dicoding.courseschedule.util.SortType
 import com.dicoding.courseschedule.util.executeThread
+import java.util.Calendar
 
 //TODO 4 : Implement repository with appropriate dao
 class DataRepository(private val dao: CourseDao) {
 
     fun getNearestSchedule(queryType: QueryType) : LiveData<Course?> {
-        throw NotImplementedError("needs implementation")
+        return dao.getNearestSchedule(nearestQuery(queryType))
     }
 
     fun getAllCourse(sortType: SortType): LiveData<PagedList<Course>> {
-        throw NotImplementedError("needs implementation")
+        return dao.getAll(sortedQuery(sortType)).toLiveData(Config(PAGE_SIZE))
     }
 
     fun getCourse(id: Int) : LiveData<Course> {
-        throw NotImplementedError("needs implementation")
+        return dao.getCourse(id)
     }
 
     fun getTodaySchedule() : List<Course> {
-        throw NotImplementedError("needs implementation")
+        return dao.getTodaySchedule(Calendar.getInstance().get(Calendar.DAY_OF_WEEK))
     }
 
     fun insert(course: Course) = executeThread {
-
+        dao.insert(course)
     }
 
     fun delete(course: Course) = executeThread {
-
+        dao.delete(course)
     }
 
     companion object {
